9e9e73c71ccd4b1c3dcd90f79ba70b8beb72f006
Taiga Saito <me@taigasaito.org>
2026-03-01 18:28:40 +0900

    chore: 同じ NW 内からアクセスできるようにした

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import path from 'node:path';
+import basicSSL from '@vitejs/plugin-basic-ssl';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
@@ -6,13 +7,13 @@ import { defineConfig } from 'vite';
 // https://vite.dev/config/
 export default defineConfig({
   base: '/khr2026_team1_bt_controller/',
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), basicSSL()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
   },
   server: {
-    allowedHosts: ['arrested-studio-forty-politics.trycloudflare.com'],
+    host: true,
   },
 });
